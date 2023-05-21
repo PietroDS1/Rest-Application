4de08dd417322799d5ca59e0bc98bbc9ace77655
PietroDS1 <danton.pietro@gmail.com>
2023-05-21 18:04:17 -0300

    Endpoint Greeting

---
+++ 05_FirstSteps/Rest-Application/src/main/java/br/com/pietro/Greeting.java
@@ -0,0 +1,24 @@
+package br.com.pietro;
+
+public class Greeting {
+
+	private final long id;
+	private final String content;
+	/**
+	 * @param id
+	 * @param content
+	 */
+	public Greeting(long id, String content) {
+		
+		this.id = id;
+		this.content = content;
+	}
+	public long getId() {
+		return id;
+	}
+	public String getContent() {
+		return content;
+	}
+	
+	
+}
